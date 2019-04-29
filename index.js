673ec32c4e09fca6bbcc20ccd9ed7818e52e6c08
Pixelhaze <harrisonjoeparker@gmail.com>
2019-04-29 19:47:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ bot.on("messageReactionAdd", (reaction) => {
     if(reaction.message.channel.name !== "apply") return;
 
     const member = reaction.users.last();
-    const user = reaction.message.guild.members.get(member.id).user;
+    const user = reaction.message.guild.members.get(member.id).displayName;
 
     if(member.bot) return;
     if(reaction.emoji.name === "🇭"){
